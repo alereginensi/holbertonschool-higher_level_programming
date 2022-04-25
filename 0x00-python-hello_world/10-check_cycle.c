ea19eac694a1992da165ab652d1617990013c4a5
alereginensi <reginensia@gmail.com>
2022-04-25 10:35:58 -0700

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * check_cycle - linked list cycle
+ * @list: list
+ * Return: 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	if (list == 0)
+	{
+		return (0);
+	}
+	return (0);
+}
