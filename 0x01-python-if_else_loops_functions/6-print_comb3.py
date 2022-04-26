b2c99cbe10348dd2a5fdc1e18a058639c3f93b0d
alereginensi <reginensia@gmail.com>
2022-04-26 10:10:57 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range((i+1), 10):
-        if (i != 8) and (j != 9):
-            print("{}{}, ".format(i,j), end="")
-
-print("89")
+        print("{}{}".format(i,j), end ="")
+        if i is not 8 or j is not 9:
+            print(end=", ")
+        if i == 8 and j == 9:
+            print()
