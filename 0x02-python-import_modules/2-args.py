3b14e1514f064ed608136f0d814c633fb9773f55
alereginensi <reginensia@gmail.com>
2022-04-28 10:53:51 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print(f"0 arguments")
+    elif len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+        print(f"{len(argv) - 1}: {argv[1]}")
+    elif len(argv) > 2:
+        print(f"{len(argv) - 1} arguments:")
+        for n in range(1, len(argv)):
+            print(f"{n}: {argv[n]}")
+        
