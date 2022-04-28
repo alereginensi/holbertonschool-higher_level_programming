8192361e5133728320f89090018a10de01d63d0b
alereginensi <reginensia@gmail.com>
2022-04-28 12:00:05 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     result = 0
     if len(argv) - 1 == 0:
         print(f"{result}")
-    for num in range(1, len(argv)):
-        result += int(argv[num])
-    print(f"{result}")
+    else:
+        for num in range(1, len(argv)):
+            result += int(argv[num])
+        print(f"{result}")
