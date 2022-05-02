db16341b4b74dc290c3e093e13271cfef4f5df6c
alereginensi <reginensia@gmail.com>
2022-05-02 09:58:02 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        my_string2 = my_string.translate({ord('c'): None})
-        my_string3 = my_string2.translate({ord('C'): None})
-        return(my_string3)
+    if my_string:
+        for i in range(len(my_string)):
+            my_string2 = my_string.translate({ord('c'): None})
+            my_string3 = my_string2.translate({ord('C'): None})
+            return(my_string3)
