30ce352a59481320fa7cc58b7c7fe677cecbcc36
alereginensi <reginensia@gmail.com>
2022-05-02 13:22:17 -0700

    task 11 try

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,8 +2,8 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return(my_list)
-    elif idx is not my_list:
+    elif idx not in range(len(my_list)):
         return(my_list)
     else:
         for idx in my_list:
-            return(idx)
+            return(my_list)
