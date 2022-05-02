e00966dc32ae2e52b938831a9db39b8e5912923b
alereginensi <reginensia@gmail.com>
2022-05-02 14:43:21 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,12 +11,12 @@ int is_palindrome(listint_t **head)
 	{
 		if ((*head)->next != NULL)
 		{
-			return (1);
+			return (0);
 			*head = (*head)->next;
 		}
 		else
 		{
-			return (0);
+			return (1);
 			*head = (*head)->next;
 		}
 	}
