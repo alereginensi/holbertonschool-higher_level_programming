99221c56ad38c6dbc2344eed74ae5a5b75721b72
alereginensi <reginensia@gmail.com>
2022-05-02 12:32:24 -0700

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,8 @@
 def multiple_returns(sentence):
     leng = len(sentence)
     first = sentence[0]
+    my_tuple = (leng, first)
     if not sentence:
         return(0, None)
     else:
-        return(leng, first)
+        return(my_tuple)
