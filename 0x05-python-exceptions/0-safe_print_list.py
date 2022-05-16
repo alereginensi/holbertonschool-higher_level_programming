9d22281b7a5f083d2215da951a8b6fd6c95f82fa
alereginensi <reginensia@gmail.com>
2022-05-16 08:14:43 -0700

    task 0 try

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for x in my_list:
+            print(x, end="")
+    except:
+        print(x)
+    print()
+    return x
