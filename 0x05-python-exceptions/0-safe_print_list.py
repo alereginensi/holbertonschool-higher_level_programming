ef3fb4405a4c1856af93ef0ddfaf2a1b2f7d0a55
alereginensi <reginensia@gmail.com>
2022-05-16 09:00:21 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     while i < x:
         try:
             print("{}".format(my_list[i]), end="")
-        except:
+        except Exception:
             break
         i += 1
     print()
