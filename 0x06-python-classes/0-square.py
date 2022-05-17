7abc64814af50244c8b1c8ba95f210c49250e3b0
alereginensi <reginensia@gmail.com>
2022-05-17 12:52:19 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-# Class Square that defines a square:
+'''Class Square that defines a square'''
 
 class Square:
-    # create an Empty class
+    '''create an Empty class'''
     def __init__(self):
-        # defines init
+        '''defines init'''
         pass
