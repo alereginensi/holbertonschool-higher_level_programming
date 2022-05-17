a480e77820c856f882c9cc87a773d17c37e712e0
alereginensi <reginensia@gmail.com>
2022-05-17 14:09:13 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''Write a class Square that defines a square by: (based on 3-square.py)'''
+
+
+class Square:
+    '''creating class square'''
+    def __init__(self, size=0):
+        '''defining size'''
+        self.__size = size
+
+    def size(self):
+        '''self size'''
+        return self.__size
+
+    def size(self, value):
+        '''defining value'''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        '''defining area operation'''
+        return self.__size * self.__size
