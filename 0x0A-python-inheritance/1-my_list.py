e44592551f949638738f219719b2ec736d31cd63
alereginensi <reginensia@gmail.com>
2022-05-30 13:24:08 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''Write a class MyList that inherits from list'''
+
+
+class MyList(list):
+    '''defining new class'''
+    def print_sorted(self):
+        '''defining print_sorted'''
+        print(sorted(self))
