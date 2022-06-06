b522c28243ea709f6e6dcab121d9555f102d57cb
alereginensi <reginensia@gmail.com>
2022-06-06 12:30:09 -0700

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,7 +14,8 @@ class Square(Rectangle):
 
     def __str__(self):
         '''returns Rectangle properties'''
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) \
+{self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
