81bf26c9e2ab9ebc20d2cfb83ded1c87ed86234d
alereginensi <reginensia@gmail.com>
2022-06-06 14:30:39 -0700

    try

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,5 +22,6 @@ class Base:
     def to_json_string(list_dictionaries):
         '''defining to_json_string'''
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            list_dictionaries = []
-        return json.dumps(list_dictionaries)
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
