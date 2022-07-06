b3c6e5a4f92dca9056f8140a314c017b35a8719d
alereginensi <reginensia@gmail.com>
2022-07-06 12:17:10 -0700

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT *.* TO 'user_0d_1'@'hbtn_0d_2';
