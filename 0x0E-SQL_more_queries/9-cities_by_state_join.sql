feaefb532d721d5b8bd2d3b0c211407501c8ab4a
alereginensi <reginensia@gmail.com>
2022-07-06 13:42:07 -0700

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
 
-SELECT cities FROM hbtn_0d_usa ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
