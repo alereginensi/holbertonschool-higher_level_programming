544c881a3d5fe7a4a6da0e66f98f3a3d24d1e326
alereginensi <reginensia@gmail.com>
2022-08-01 03:18:42 -0700

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// script that prints 3 lines
+
+const myVar = ['JavaScript is amazing', 'C is fun', 'Python is cool'];
+
+console.log(myVar[1]);
+console.log(myVar[2]);
+console.log(myVar[0]);
