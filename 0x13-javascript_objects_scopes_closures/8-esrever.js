7365c62e8ffca9214568e5f7f3436824e9233de5
alereginensi <reginensia@gmail.com>
2022-08-02 08:27:06 -0700

    task 8 fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  for (let i = 0; i < list.length - 2; i++) {
-    list.splice(i, 0, list.pop());
+  for (let i = 0, j = list.length - 1; i < j; i++, j--) {
+    [list[i], list[j]] = [list[j], list[i]];
   }
   return list;
 };
