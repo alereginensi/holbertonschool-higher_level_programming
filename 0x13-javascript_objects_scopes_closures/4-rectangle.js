bc24951d31a32a5a5d7dde68d981f8817a4b83c8
alereginensi <reginensia@gmail.com>
2022-08-02 08:21:24 -0700

    task 4 fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,17 +10,17 @@ class Rectangle {
 
   print () {
     for (let n = 0; n < this.height; n++) console.log('X'.repeat(this.width));
-}
-
-  double () {
-    this.width *= 2;
-    this.width *= 2;
   }
 
   rotate () {
     const rotation = this.width;
     this.width = this.height;
     this.height = rotation;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 module.exports = Rectangle;
