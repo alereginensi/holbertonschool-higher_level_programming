0f1e84723f7e467f64957811f425eed93bb288d9
alereginensi <reginensia@gmail.com>
2022-08-18 20:14:12 +0000

    task 5 fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST "$1" -d "email=test@gmail.com" -d "Subject=I will always be here for PLD"
+curl -sX POST -F "email=test@gmail.com" -F "subject=I will always be here for PLD" "$1"
