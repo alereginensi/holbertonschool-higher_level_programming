9083dd6726c67ccde14f764edfd073bc85610770
alereginensi <reginensia@gmail.com>
2022-08-18 19:03:11 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s https://code.jquery.com/jquery-3.1.1.min.js | wc -c
+curl -sIw size_request "$1" | grep Content-Length | awk '{print $2}'
