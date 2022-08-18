aba844940c6e7a4b055743f1061a6bbc1b1b0c1e
alereginensi <reginensia@gmail.com>
2022-08-18 18:17:04 +0000

    push

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI $URL | grep -i Content-Length | awk '{print $2}'
+curl -s https://code.jquery.com/jquery-3.1.1.min.js | wc -c
