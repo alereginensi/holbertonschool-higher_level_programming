7fb78de73a2b103c6044b9547f45d5290a910c64
alereginensi <reginensia@gmail.com>
2022-09-12 13:25:40 -0700

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+try {
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
