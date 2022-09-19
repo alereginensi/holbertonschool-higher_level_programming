fa2ee31b23b47447a75b3e0dfbd24167816cc63c
alereginensi <reginensia@gmail.com>
2022-09-19 11:59:43 -0700

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+// script that fetches and lists the title for all movies by using this URL
+// https://swapi-api.hbtn.io/api/films/?format=json
+
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (movie) {
+    const movies = movie.results;
+    for (const x in movies) {
+  $('UL#list_movies').append('<li>' + movies[x].title + '</li>');
+    }
+});
