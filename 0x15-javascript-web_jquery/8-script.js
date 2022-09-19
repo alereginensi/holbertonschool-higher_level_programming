828c5ecdad608dc1caf678861bc8581439b18bf7
alereginensi <reginensia@gmail.com>
2022-09-19 12:11:12 -0700

    fixed semistandard

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -2,8 +2,8 @@
 // https://swapi-api.hbtn.io/api/films/?format=json
 
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (movie) {
-    const movies = movie.results;
-    for (const x in movies) {
-  $('UL#list_movies').append('<li>' + movies[x].title + '</li>');
-    }
+  const movies = movie.results;
+  for (const x in movies) {
+    $('UL#list_movies').append('<li>' + movies[x].title + '</li>');
+  }
 });
