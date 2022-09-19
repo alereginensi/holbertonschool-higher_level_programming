5fb8fad8bc6ca190faac0438764d53fb2566fab3
alereginensi <reginensia@gmail.com>
2022-09-19 11:20:55 -0700

    task 5

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,6 @@
+// script that adds a <li> element to a list when the user clicks on the tag DIV#add_item
+// https://www.tutorialrepublic.com/faq/how-to-add-li-in-an-existing-ul-using-jquery.php
+
+$("DIV#add_item").click(function(){
+    $("UL.my_list").append('<li>Item</li>');
+});
